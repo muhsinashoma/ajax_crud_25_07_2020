dbdd3104d380813ff31fddfce2541a3af2ad7ad5
muhsina.akter <muhsina.akter2@gmail.com>
2020-07-25 11:17:26 +0600

    CRUD Design and Form Value insert into the database are done

---
+++ app/db.php
@@ -0,0 +1,18 @@
+<?php
+
+
+	/**
+	 * Database connection
+	 */
+
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db = 'ac';
+
+	$connection = new mysqli($host, $user,$pass,$db);
+
+
+
+
+?>
